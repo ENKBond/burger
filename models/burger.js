79d0a408f633316bb24b29cc473b42406cabd598
ENK Bond <43937176+ENKBond@users.noreply.github.com>
2019-02-24 12:06:44 -0700

    updates

--- models/burger.js
+++ models/burger.js
@@ -19,12 +19,12 @@ const burger = {
         orm.insert("burgers", cols, vals, function (res) {
             cb(res);
         });
-    },
-    delete: function(condition, cb) {
-        orm.delete("burgers", condition, function(res) {
-          cb(res);
-        });
     }
+    // delete: function(condition, cb) {
+    //     orm.delete("burgers", condition, function(res) {
+    //       cb(res);
+    //     });
+    // }
 };
 
 module.exports = burger;
